1727ae18715c09277294c48e746fbeb5ed14c212
Greg Gzik <gregory_gzik@acl.com>
2023-08-23 20:15:41 +0000

    Development Code Changes

--- src/CreateExpense/CreateExpenseHandler.js
+++ src/CreateExpense/CreateExpenseHandler.js
@@ -1,3 +1,41 @@
+import AWS from 'aws-sdk';
+import * as uuid from 'uuid';
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient({ region: 'us-west-2' });
+
 export const handler = async (event, context) => {
-    console.log("Hello World!");
+    const body = JSON.parse(event.body);
+    const id = uuid.v4();
+
+    if (!body.date || !body.location || !body.amount || !body.distance) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: "Required field(s) missing or invalid" })
+        }
+    }
+
+    const expense = {
+        TableName: "Expenses",
+        Item: {
+            id,
+            date: body.date,
+            location: body.location,
+            distance: body.distance,
+            amount: body.amount
+        }
+    };
+
+    try {
+        await dynamoDb.put(expense).promise();
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ message: "Expense saved successfully." })
+        };
+    } catch (error) {
+        console.error("Error saving to DynamoDB:", error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: "Internal Server Error" })
+        };
+    }
 };
--- src/CreateExpense/CreateExpenseHandler.spec.js
+++ src/CreateExpense/CreateExpenseHandler.spec.js
@@ -1,7 +1,65 @@
-import { handler } from './CreateExpenseHandler';
+import AWSMock from 'aws-sdk-mock';
+import { jest } from '@jest/globals';
+
+jest.setTimeout(10_000);
 
 describe('CreateExpense', () => {
-    it('Does something', () => {
-        handler();
+    async function setupMocks() {
+        const mockPut = jest.fn((_, result) => result);
+        AWSMock.mock('DynamoDB.DocumentClient', 'put', mockPut);
+
+        const handler = (await import('./CreateExpenseHandler')).handler;
+
+        return {
+            handler,
+            mockPut
+        };
+    }
+
+    it('saves the expense to DynamoDB', async () => {
+        const { handler, mockPut } = await setupMocks();
+
+        const eventData = {
+            body: JSON.stringify({
+                date: '2023-08-25',
+                location: 'Toronto',
+                distance: 120,
+                amount: 50
+            })
+        };
+
+        const response = await handler(eventData);
+        
+        expect(mockPut).toHaveBeenCalledWith(expect.objectContaining({
+            TableName: "Expenses",
+            Item: {
+                id: expect.any(String),
+                date: '2023-08-25',
+                location: 'Toronto',
+                distance: 120,
+                amount: 50
+            }
+        }), expect.anything());
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toEqual(JSON.stringify({ message: "Expense saved successfully." }));
     });
-})
+
+    it('should return a 400 error if required fields are missing', async () => {
+        const { handler } = await setupMocks();
+
+        const mockEventWithMissingFields = {
+            body: JSON.stringify({
+                date: "2023-08-24",
+                location: "Toronto",
+                amount: 45.00
+                // "distance" field is intentionally left out
+            })
+        };
+        
+        const response = await handler(mockEventWithMissingFields);
+    
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toEqual(JSON.stringify({ message: "Required field(s) missing or invalid" }));
+    });
+});
